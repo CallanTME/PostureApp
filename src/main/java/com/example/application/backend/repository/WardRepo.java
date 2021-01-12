4f7dd03c681e32969190d109f9071166f09c3ca5
aliceDuhem <lili.duhem@gmail.com>
2021-01-12 21:39:06 +0000

    Enter nurse with ward name

--- src/main/java/com/example/application/backend/repository/WardRepo.java
+++ src/main/java/com/example/application/backend/repository/WardRepo.java
@@ -1,7 +1,14 @@
 package com.example.application.backend.repository;
 
+import com.example.application.backend.entity.Nurse;
 import com.example.application.backend.entity.Ward;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface WardRepo extends JpaRepository<Ward,Long> {
+    @Query("select w from Ward w " +
+            "where ward_name like concat('%', :name, '%') ")
+    //For future iteration, have the hospitalId in too; this page should be accessible only for the staff of a specific hospital
+    Ward search(@Param("name") String nameToFind);
 }
